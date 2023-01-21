a058f97d9822f232addbce1c0d8e9c00388489bc
lytex <julianlpc15@gmail.com>
2023-01-21 18:43:58 +0100

    rename and use json structure

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
-# KeepToOrg
+# Keep2Org
+
 Convert Google Keep notes to Emacs .org files
 
 Usage:
-`python KeepToOrg.py /path/to/google/Keep output/dir`
+`python k2o.py /path/to/google/Keep/folder /path/to/org/file.org`
 
-Given a Takeout of your Google Keep Notes in .html format, output .org files with logical groupings 
+Given a Takeout of your Google Keep Notes in .html format, output .org files with logical groupings
 based on tags. This will also format lists and try to be smart.
 
 Go to [Google Takeout](https://takeout.google.com/settings/takeout) to download your Keep data. Extract the data and run this script on it.
-
-Note that the dates used to sort the notes can be out of order. After investigating, the dates in the note .html files themselves appear to be wrong. I think this is Google's fault.
---
+++ k2o.py
@@ -0,0 +1,60 @@
+import datetime
+import json
+import sys
+from glob import glob
+
+# Example json structure
+{
+    "color": "DEFAULT",
+    "isTrashed": False,
+    "isPinned": False,
+    "isArchived": False,
+    "textContent": "Note content",
+    "title": "Note title, empty if dictated from Android Auto",
+    "userEditedTimestampUsec": 1673456350742000,
+    "createdTimestampUsec": 1673456350742000,
+    "labels": [{"name": "tag2"}, {"name": "tag1"}],  # This can be empty if there is no tag
+}
+
+
+def main(keep_dir, org_file):
+    notes = []
+    for file in glob(keep_dir + "/*/*.json"):
+        with open(file) as f:
+            contents = json.load(f)
+            notes.append(contents)
+    # Sort by edition time desc
+    notes = sorted(notes, key=lambda x: -x["userEditedTimestampUsec"])
+    org_contents = "\n".join(map(generate_headline, notes))
+    with open(org_file, "w") as f:
+        f.write(org_contents)
+
+
+def generate_headline(note):
+    tags = ["ARCHIVE"] * note["isArchived"]
+    created = datetime.datetime.fromtimestamp(note["userEditedTimestampUsec"] / 1e6).strftime("[%Y-%m-%d %a %H:%M]")
+    properties = f":PROPERTIES:\n:CREATED: {created}\n:END:"
+
+    if note.get("labels"):
+        tags += [x["name"] for x in note["labels"]]
+    if tags != []:
+        tags = " :" + ":".join(tags) + ":"
+    else:
+        tags = ""
+
+    if not note["isTrashed"]:
+        if note["title"] == "":
+            return f"* {note['textContent']}{tags}\n{properties}"
+        else:
+            return f"* {note['title']}{tags}\n{properties}\n{note['textContent']}"
+    else:
+        return ""
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Wrong number of arguments!\nUsage:\n\tpython k2o.py /path/to/google/Keep/folder /path/to/org/file.org")
+    else:
+        keep_dir = sys.argv[1]
+        org_file = sys.argv[2]
+        main(keep_dir, org_file)
